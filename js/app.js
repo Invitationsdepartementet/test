c2b92fab7e7e36db9a2c2ffbbd41ce9bb859d076
Carlos Egusquiza <carlosea@kth.se>
2017-11-21 14:56:17 +0100

    Trim javascript

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
 $(function() {
 
-  $destinationButton = $(".js-go-to-dinner");
-
   $(".js-button-play-movie").click(function(e) {
     e.preventDefault();
     $(".js-video-overlay")
@@ -13,37 +11,6 @@ $(function() {
     $(".js-vimeo-iframe").remove();
     $(this).removeClass("video-overlay-active");
   });
-
-  $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
-      selectCityWithNameOrRegion(data.city, data.regionName);
-  });
-
-  function selectCityWithNameOrRegion(cityName, cityRegion) {
-    // Find a city with an identical name in the option group, and select it
-    var selection = $(".js-cities-group option[value='" + cityName +"']");
-
-    // If no city is available with the same name, use the region as a name (could be .e.g Stockholm)
-    if (!selection.length && cityRegion) {
-      selection = $(".js-cities-group option[value='" + cityRegion +"']");
-    }
-    
-    // If no selection, abort
-    if (!selection.length) { return; }
-
-    // Set it to selected
-    selection.prop('selected', true);
-    updateDestinationButtonFromSelectedOption();
-  }
-
-  $(".js-destination-dropdown").change(function(e) {
-    updateDestinationButtonFromSelectedOption();
-  });
-
-  // Update CTA button to go to page for chosen destination
-  function updateDestinationButtonFromSelectedOption() {
-    var selected = $(".js-destination-dropdown option:selected");
-    $destinationButton.attr("href", selected.attr("data-url"));
-  }
 
   // Heatmap
   heatmap_ext = {
